915aa6cb4ad4981c239d707312ad86418989d093
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-28 16:02:53 +0200

    implemented contact page and footer

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const burgerMenu = document.querySelector(".burger");
 const navLinks = document.querySelectorAll(".nav-links");
+const navSection = document.querySelectorAll(".nav-links li");
+console.log(navSection);
 
-burgerMenu.addEventListener("click", (event) => {
+burgerMenu.addEventListener("click", () => {
   navLinks.forEach((link) => {
     link.classList.toggle("burger-active");
+  });
+});
+
+navSection.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    navLinks.forEach((link) => {
+      link.classList.remove("burger-active");
+    });
   });
 });

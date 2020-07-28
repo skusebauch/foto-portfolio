1b97718b8209a55db196c2ff105c2ee1f1cfb379
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-07-28 16:30:42 +0200

    refactor js

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-const burgerMenu = document.querySelector(".burger");
-const navLinks = document.querySelectorAll(".nav-links");
-const navSection = document.querySelectorAll(".nav-links li");
-console.log(navSection);
+//const burgerMenu = document.querySelector(".burger");
+//const navLinks = document.querySelectorAll(".nav-links");
+//const links = document.querySelectorAll(".nav-links li");
+//console.log(navSection);
 
-burgerMenu.addEventListener("click", () => {
-  navLinks.forEach((link) => {
-    link.classList.toggle("burger-active");
-  });
+//burgerMenu.addEventListener("click", () => {
+//  navLinks.forEach((link) => {
+//    link.classList.toggle("burger-active");
+//  });
+//});
+
+//navSection.forEach((link) => {
+//  link.addEventListener("click", (event) => {
+//    navLinks.forEach((link) => {
+//      link.classList.remove("burger-active");
+//    });
+//  });
+//});
+
+// refactor
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
 });
 
-navSection.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    navLinks.forEach((link) => {
-      link.classList.remove("burger-active");
-    });
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
   });
 });
